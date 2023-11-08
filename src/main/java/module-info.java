146bab73eb7fbe2bc15e03436570ca9b1ad02d8c
Moaath Alrajab <alrajam@farmingdale.edu>
2023-11-08 13:44:20 -0500

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,22 @@
+module com.example.csc325_firebase_webview_auth {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    requires jdk.jsobject;
+    requires java.xml;
+    requires java.logging;
+    requires javafx.web;
+    requires com.google.auth.oauth2;
+    requires google.cloud.firestore;
+    requires firebase.admin;
+    requires com.google.api.apicommon;
+
+    requires google.cloud.core;
+    requires com.google.auth;
+
+
+    opens com.example.csc325_firebase_webview_auth;
+    exports com.example.csc325_firebase_webview_auth;
+    opens com.example.csc325_firebase_webview_auth.modelview;
+    exports com.example.csc325_firebase_webview_auth.modelview;
+}
